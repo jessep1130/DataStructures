10e0537b29f44977d808c80e2cf38e4769b349f6
Jesse Perez <jessep1130@gmail.com>
2021-12-19 20:14:17 -0600

    more linkedList practice

---
+++ Snippets/linkedList_sumList.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor(val){
+
+        this.val = val;
+        this.next = null; 
+    }
+}
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+
+
+a.next = b;
+b.next = c;
+c.next = d; 
+
+const sumList = (head) => {
+    let sum = 0;
+    let current = head; 
+    while (current !== null){
+        sum += current.val;
+        current = current.next; 
+        // this line is causing us to traverse that linkedList 
+    }
+    return sum; 
+};
+
+sumList(a); 
